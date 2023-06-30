9f2a278eb10cd103d40d830f92093b265abefa08
Zakaria <z.lezgham@gmail.com>
2023-06-30 17:24:18 +0200

    angular

---
+++ PaginationSignal.js
@@ -0,0 +1,43 @@
+import '../styles/Tod.css';
+import ReactPaginate from 'react-paginate';
+import { useState, useEffect, useRef } from 'react';
+import { useLocalState } from '../util/useLocalState';
+import { complex } from 'framer-motion';
+
+const PaginationSignal = ({setPage, maxItems}) => {
+    const [activePage, setActivePage] = useLocalState(0, "activePageSignal");
+    useEffect(() => {
+        const value = JSON.parse("["+localStorage.getItem("activePageSignal")+"]");
+            return activePage !== 0? 
+            setActivePage(value) : setActivePage([0]);
+            }
+                , [maxItems]);
+
+    const handlePageClick = (e) => {
+        setPage(e.selected);
+        setActivePage(e.selected);
+    }
+return (
+    <ReactPaginate 
+                    breakLabel={"..."}
+                    nextLabel={">>"}
+                    previousLabel={"<<"} 
+                    pageCount={maxItems}
+                    marginPagesDisplayed={3}
+                    pageRangeDisplayed={5}
+                    onPageChange={handlePageClick}
+                    containerClassName={"pagination justify-content-center"}
+                    pageClassName={"page-item"}
+                    pageLinkClassName={"page-link"}
+                    previousClassName={"page-item"}
+                    previousLinkClassName={"page-link"}
+                    nextClassName={"page-item"}
+                    nextLinkClassName={"page-link"}
+                    breakClassName={"page-item"}
+                    breakLinkClassName={"page-link"}
+                    activeClassName={"page-item active"}
+                    forcePage={activePage[0]}
+                    />
+)
+}
+export default PaginationSignal
